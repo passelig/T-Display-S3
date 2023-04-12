6670c6743a839421e186f6b256761624865e19a4
Gunnar Storebø <gunnar.storebo@vlfk.no>
2023-04-12 12:34:31 +0200

    Added HTTP-GET : Writing two lines to display

---
+++ LilygoS3-HTTP-GET/LilygoS3-HTTP-GET.ino
@@ -0,0 +1,26 @@
+//#include "install.h"
+#include "TFT_eSPI.h"
+#include <DHT.h>
+
+TFT_eSPI tft= TFT_eSPI();
+TFT_eSprite sprite = TFT_eSprite(&tft);
+
+void setup() {
+  tft.init();
+  tft.setRotation(1);
+  tft.setSwapBytes(true);
+  tft.fillScreen(TFT_WHITE);
+  sprite.createSprite(320,170);
+  sprite.setTextColor(TFT_BLACK,TFT_WHITE);
+}
+
+
+
+void loop() {
+ sprite.fillSprite(TFT_WHITE);
+ sprite.setFreeFont(&Orbitron_Light_24);
+ sprite.drawString("Some long string 10,10",10,10);
+ sprite.drawString("Short 10,44",10,44); 
+ sprite.pushSprite(0,0);
+ delay(100);
+}
