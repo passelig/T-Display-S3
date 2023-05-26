ba3a0eeb74e53b281170fca2c6e264d1a5841df6
Gunnar Storebø <gunnar.storebo@vlfk.no>
2023-05-26 17:50:54 +0200

    Light_mode

--- LilygoS3-TemperatureGraph-DHT22/LilygoS3-TemperatureGraph-DHT22.ino
+++ LilygoS3-TemperatureGraph-DHT22/LilygoS3-TemperatureGraph-DHT22.ino
@@ -14,11 +14,12 @@ float current=0;
 float pixelsPerDegree = gh /12;
 int yStartTemp = 18;
 
-#define gray 0x6B6D
+#define grey 0xDF7E
 #define blue 0x0967
 #define orange 0xC260
 #define purple 0x604D
 #define green 0x1AE9
+#define black 0x0000
 DHT dht(16,DHT22);    
 
 
@@ -53,7 +54,7 @@ void loop()
  }
  measurmentPoints[numOfValues-1]=current;
 
-  sprite.fillSprite(TFT_BLACK);
+  sprite.fillSprite(TFT_WHITE);
   sprite.setTextColor(TFT_WHITE,blue);
   sprite.fillRoundRect(6,5,250,35,4,blue);
   sprite.drawString("Temperature " + String(current),10,24,4);
@@ -61,16 +62,17 @@ void loop()
 
   // Vertical grid lines
   for(int i=1;i<20;i++){
-   sprite.drawLine(gx+(i*17),gy,gx+(i*17),gy-gh,gray);
+   sprite.drawLine(gx+(i*17),gy,gx+(i*17),gy-gh,grey);
   }
   // Horizontal grid lines
+sprite.setTextColor(TFT_BLACK,TFT_WHITE);  
   for(int i=1;i<6;i++){
-   sprite.drawLine(gx,gy-(i*17),gx+gw,gy-(i*17),gray);
+   sprite.drawLine(gx,gy-(i*17),gx+gw,gy-(i*17),grey);
    sprite.drawString(String(yStartTemp+(i*2)),gx-16,gy-(i*17));
   }
 
-  sprite.drawLine(gx,gy,gx+gw,gy,TFT_WHITE);  //Horizontal axis line
-  sprite.drawLine(gx,gy,gx,gy-gh,TFT_WHITE);    // Vertical axis line
+  sprite.drawLine(gx,gy,gx+gw,gy,TFT_BLACK);  //Horizontal axis line
+  sprite.drawLine(gx,gy,gx,gy-gh,TFT_BLACK);    // Vertical axis line
   
   // Draw the graph  
   for(int i=0;i<numOfValues;i++){
