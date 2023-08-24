6b0418bda51d1fd695151cfea47a5cd56e20bedd
Gunnar Storebø <gunnar.storebo@vlfk.no>
2023-08-24 09:18:41 +0200

    Remember to let Lilygo rest before powering with battery

--- LilygoS3-Car/LilygoS3_UltrasonicSensorAndServo/LilygoS3_UltrasonicSensorAndServo.ino
+++ LilygoS3-Car/LilygoS3_UltrasonicSensorAndServo/LilygoS3_UltrasonicSensorAndServo.ino
@@ -1,4 +1,9 @@
 #include <ESP32Servo.h>
+#include "TFT_eSPI.h"
+
+TFT_eSPI tft= TFT_eSPI();
+TFT_eSprite sprite = TFT_eSprite(&tft);
+
 int trigPin = 12;
 int echoPin = 13;
 int servoPin = 11;
@@ -13,11 +18,28 @@ Servo servoMotor;
 
 void setup()
 { 
+  tft.init();
+  tft.setRotation(1);
+  tft.setSwapBytes(true);
+  sprite.createSprite(320,170);
+  sprite.setTextColor(TFT_BLACK,TFT_WHITE);
+  sprite.fillSprite(TFT_WHITE);
+  sprite.setFreeFont(&Orbitron_Light_24);
+  sprite.drawString("LILYGO EXT 5V",60,24); 
+  sprite.drawString("Ultrasonic Sensor",30,74); 
+  sprite.drawString("with Servoz",80,104); 
+  sprite.pushSprite(0,0);
+
+  pinMode(15, OUTPUT); // to boot with battery...
+  digitalWrite(15, 1);  // and/or power from 5v rail instead of USB
+
   pinMode(trigPin,OUTPUT);
   pinMode(echoPin,INPUT);
   Serial.begin(115200);
   servoMotor.attach(servoPin, 500, 2500);
   servoMotor.write(straitAhead);
+
+  delay(1000);
 }
 
 void loop()
