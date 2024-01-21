ac069a7e1bf04177532dd4a9209175688d9aa892
Gunnar Storebø <gunnar.storebo@vlfk.no>
2024-01-21 22:58:30 +0100

    Adapted to showing cm

---
+++ LilygoS3_Modbus_Ultrasonic_Graph/LilygoS3_Modbus_Ultrasonic_Graph.ino
@@ -0,0 +1,87 @@
+#include "Arduino.h"
+#include "TFT_eSPI.h" /* Please use the TFT library provided in the library. */
+#include "DHT.h"
+
+TFT_eSPI lcd = TFT_eSPI();
+TFT_eSprite sprite = TFT_eSprite(&lcd);
+#define numOfValues 145
+float measurmentPoints[numOfValues] = { 0 };
+int gw = 294;
+int gh = 102;
+int gx = 25;
+int gy = 154;
+int current = 0;
+float pixelsPerCentimeter = gh / 240.0;
+int yStartTemp = 0;
+int personCount = 0;
+
+#define grey 0xDF7E
+#define blue 0x0967
+#define orange 0xC260
+#define purple 0x604D
+#define green 0x1AE9
+#define black 0x0000
+DHT dht(16, DHT22);
+
+void setup(void) {
+  dht.begin();
+  Serial.begin(115200);
+  lcd.init();
+  lcd.fillScreen(TFT_BLACK);
+  lcd.setRotation(1);
+
+  sprite.createSprite(320, 170);
+  sprite.setTextDatum(3);
+  sprite.setSwapBytes(true);
+  measurmentPoints[numOfValues - 1] = yStartTemp;
+  analogReadResolution(10);
+  current = 150;  //dht.readTemperature();
+  for (int i = 0; i < numOfValues; i++) {
+    measurmentPoints[i] = current;
+  }
+}
+
+
+
+void loop() {
+  current = 200-personCount;  //dht.readTemperature();
+  if (personCount>100){
+    personCount = 0;
+  }
+
+
+  for (int i = 0; i < numOfValues; i++) {
+    measurmentPoints[i] = measurmentPoints[i + 1];
+  }
+  measurmentPoints[numOfValues - 1] = current;
+
+  sprite.fillSprite(TFT_WHITE);
+  sprite.setTextColor(TFT_WHITE, blue);
+  sprite.fillRoundRect(6, 5, 170, 35, 4, blue);
+  sprite.drawString("Distance " + String(current), 14, 24, 4);
+  sprite.fillRoundRect(180, 5, 130, 35, 4, blue);
+  sprite.drawString("Count " + String(personCount), 190, 24, 4);
+  sprite.setFreeFont();
+
+  // Vertical grid lines
+  for (int i = 1; i < 20; i++) {
+    sprite.drawLine(gx + (i * 17), gy, gx + (i * 17), gy - gh, grey);
+  }
+  // Horizontal grid lines
+  sprite.setTextColor(TFT_BLACK, TFT_WHITE);
+  for (int i = 1; i < 7; i++) {
+    sprite.drawLine(gx , gy - (i * 17), gx  + gw, gy - (i * 17), grey);
+    sprite.drawString(String(i * 40), gx - 20, gy - (i * 17));
+  }
+
+  sprite.drawLine(gx, gy, gx + gw, gy, TFT_BLACK);      //Horizontal axis line
+  sprite.drawLine(gx , gy, gx , gy - gh, TFT_BLACK);  // Vertical axis line
+
+  // Draw the graph
+  for (int i = 0; i < numOfValues - 1; i++) {
+    sprite.drawLine(gx + (i * 2), gy - (measurmentPoints[i] - yStartTemp) * pixelsPerCentimeter, gx + ((i + 1) * 2), gy - (measurmentPoints[i + 1] - yStartTemp) * pixelsPerCentimeter, TFT_RED);
+  }
+  sprite.pushSprite(0, 0);
+  delay(1);
+  personCount++;
+}
